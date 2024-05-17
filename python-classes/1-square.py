f10e031d211930d96eca11d1279c51565d733b70
TanguyMoreau99 <tanguymoreau.aes@gmail.com>
2024-05-17 12:35:06 +0200

    task 1 fix documentation

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
-
 """
 1-square.py
 
-Add the size of the class
+This module initializes a class called 'Square' who
+contains a private instance attribute: 'size'.
 """
 
 
 class Square:
+    """
+    The 'Square' class represents a generic square.
+
+    Attributes:
+        __size (int): The size of a square.
+    """
 
     def __init__(self, size):
+        """
+        Initialize a private instance attribute: 'size'.
+
+        Args:
+            __size (int): The size of a square.
+        """
         self.__size = size
-
-
-"""
-Add the size of the class
-"""
