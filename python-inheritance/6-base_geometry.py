9489b50feba2448fcf48efcdd85b1ed9f18b751c
TanguyMoreau99 <tanguymoreau.aes@gmail.com>
2024-05-24 10:30:09 +0200

    task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module for BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """
+    Base class for geometry objects
+    """
+    def area(self):
+        """
+        Public instance method that raises an Exception
+        """
+        raise Exception("area() is not implemented")
