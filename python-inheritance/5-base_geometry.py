b700750c3a782cd97bde90223faae68d9b436e7a
TanguyMoreau99 <tanguymoreau.aes@gmail.com>
2024-05-24 10:24:03 +0200

    task 5

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module for BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """
+    A class that represents base geometry.
+    Currently, it does not have any attributes or methods.
+    """
+    pass
