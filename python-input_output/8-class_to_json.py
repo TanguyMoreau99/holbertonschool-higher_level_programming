d4e9ec87b69d59a213a573931e72aa271b0fadf5
TanguyMoreau99 <tanguymoreau.aes@gmail.com>
2024-05-27 16:17:19 +0200

    task 8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
 
 def class_to_json(obj):
+    """
+    Renvoie le dictionnaire de l'objet `obj` qui peut être facilement
+    sérialisé en JSON.
 
+    Args:
+        obj: L'objet dont on veut obtenir le dictionnaire de l'attribut.
+
+    Returns:
+        dict: Un dictionnaire contenant tous les attributs de l'objet `obj`.
+    """
+    
+    # Utilise la fonction `vars` pour obtenir le dictionnaire __dict__ de l'objet `obj`.
+    # __dict__ est un attribut spécial qui contient tous les attributs de l'objet sous forme de dictionnaire.
     json_obj = vars(obj)
+    
+    # Retourne le dictionnaire obtenu
     return json_obj
