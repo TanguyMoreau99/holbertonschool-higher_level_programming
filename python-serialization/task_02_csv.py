c6c0f9cd2b1dae143b2ae4a15f895e60e1d3251b
TanguyMoreau99 <tanguymoreau.aes@gmail.com>
2024-05-29 10:46:23 +0200

    task 2

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Reading data from one format (CSV) and converting it
+into another format (JSON) using serialization techniques
+"""
+
+import csv
+import json
+
+
+def convert_csv_to_json(csv_filename):
+    """
+    Convert CSV data to JSON format and write to data.json file.
+
+    :param csv_filename: The filename of the input CSV file
+    :return: True if the conversion was successful, False otherwise
+    """
+    try:
+        with open(csv_filename, 'r', encoding='utf-8') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            data = [row for row in csv_reader]
+
+        with open('data.json', 'w', encoding='utf-8') as json_file:
+            json.dump(data, json_file, ensure_ascii=False, indent=4)
+        return True
+
+    except Exception as e:
+        print("Error: {}".format(e))
+        return False
