aa81f8b208028153e4f1c117dcbfd6ff335dd436
TanguyMoreau99 <tanguymoreau.aes@gmail.com>
2024-05-31 14:25:57 +0200

    task 2

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,5 +1,38 @@
 #!/usr/bin/env python3
-from task_02_requests import fetch_and_print_posts, fetch_and_save_posts
+import requests
+import csv
 
-fetch_and_print_posts()
-fetch_and_save_posts()
+
+def fetch_and_print_posts():
+    url = 'https://jsonplaceholder.typicode.com/posts'
+    response = requests.get(url)
+
+    print(f'Status Code: {response.status_code}')
+
+    if response.status_code == 200:
+        posts = response.json()
+        for post in posts:
+            print(post['title'])
+
+
+def fetch_and_save_posts():
+    url = 'https://jsonplaceholder.typicode.com/posts'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        posts = response.json()
+
+        # Create a list of dictionaries with id, title, and body
+        structured_data = [
+            {'id': post['id'], 'title': post['title'], 'body': post['body']}
+            for post in posts
+            ]
+
+        # Define the CSV file column names
+        fieldnames = ['id', 'title', 'body']
+
+        # Write to CSV file
+        with open('posts.csv', mode='w', newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(structured_data)
