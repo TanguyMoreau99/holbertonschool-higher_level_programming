cadc94244e5c2aed9ea5ff879e65e78da1b1251c
TanguyMoreau99 <tanguymoreau.aes@gmail.com>
2024-06-17 15:26:46 +0200

    task 4

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+-- Create a table first_table with two columns id (integer) and name
+-- (string), if he doesn't exist, in hbtn_0c_0 database
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
